7818520fda9b7adca466c7be73d91fc66c751ed8
Nac101 <120043682+Nac101@users.noreply.github.com>
2024-01-16 16:36:38 -0300

    Update process_images.py

--- process_images.py
+++ process_images.py
@@ -4,8 +4,8 @@ import os
 def process_image(file_path):
     with Image.open(file_path) as img:
         # Calcular la nueva altura manteniendo la proporción
-        new_height = int((190.0 / img.width) * img.height)
-        img = img.resize((190, new_height), Image.Resampling.LANCZOS)
+        new_height = int((100.0 / img.width) * img.height)
+        img = img.resize((100, new_height), Image.Resampling.LANCZOS)
         img.save(file_path)
 
 for root, _, files in os.walk('.'):
